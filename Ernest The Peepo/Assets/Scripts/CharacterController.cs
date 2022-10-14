a2ecd723ac0b24553c5e72fdcd17b958e249d86f
pr-xtrf <piotr.ruszczak@xtrf.eu>
2022-10-15 00:39:35 +0200

    basic movement

--- Ernest The Peepo/Assets/Scripts/CharacterController.cs
+++ Ernest The Peepo/Assets/Scripts/CharacterController.cs
@@ -1,21 +1,36 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
-    [SerializeField] private Rigidbody2D rb;
-    private Rigidbody2D rb2;
+    private Rigidbody2D myRigidBody;
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        rb2 = GetComponent<Rigidbody2D>();
+        myRigidBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+       
+    }
+
+    private void FixedUpdate()
+    {
+        MoveCharacter();
+    }
+
+    private void MoveCharacter()
+    {
+        Vector2 movement = new Vector2(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical"));
+        //(1,0)
+        myRigidBody.MovePosition(myRigidBody.position+movement*Time.deltaTime);
         
+        // myRigidBody.MovePosition();
     }
 }
